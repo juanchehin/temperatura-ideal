29aad7e0d39f3027966a75e9eb86a38b3f5e1c9f
chehin <chehinjuan@gmail.com>
2021-11-26 10:50:11 -0300

    modificacoin estilo

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const path = require('path');
 
+// Archivos estaticos
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/views/pages/index.html'));
 })
+
 
 // Servidor y puerto
 const host = process.env.HOST || '0.0.0.0';
