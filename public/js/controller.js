bb655c79f6989a53c6642452ee2e6a66ad14bd3c
chehin <chehinjuan@gmail.com>
2021-11-28 11:17:25 -0300

    Buscar ciudad al hacer 'enter'

--- public/js/controller.js
+++ public/js/controller.js
@@ -45,3 +45,13 @@ function mostrarCartelError() {
         'error'
     )
 }
+
+texto.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("btnCiudad").click();
+    }
+});
